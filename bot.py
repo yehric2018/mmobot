26074e4b349c3e03c7d7483214188d1b0a1b2bdd
yehric2018 <eric.yehj@outlook.com>
2022-09-23 16:18:12 -0700

    Added basic bot command and configuration

--- bot.py
+++ bot.py
@@ -1,20 +1,43 @@
 import os
+import random
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+    print(f'Sent welcome message to {bot.user.name}')
+
+@bot.command(name='me')
+async def me_command(context):
+    print('got a me command')
+    await context.send(f'Your name is {context.author.nick}')
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+bot.run(TOKEN)
