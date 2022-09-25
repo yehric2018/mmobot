5d4c4c4f850b1d8de6d42475f420d5f67731923e
yehric2018 <eric.yehj@outlook.com>
2022-09-25 00:13:23 -0700

    Refactored hit simulation to use Player model

--- simulations/hit_simulator.py
+++ simulations/hit_simulator.py
@@ -1,59 +1,146 @@
+from datetime import datetime
+import os
 import random
 
-NEW_PLAYER_1 = {
-    'hp': 100,
-    'max_hp': 100,
-    'armor': 1,
-    'mobility': 25,
-    'dexterity': 25,
-    'endurance': 100,
-    'max_endurance': 100,
-    'strength': 15,
-    'luck': 1,
-    'sword_skill': 0,
-    'armor_coverage': 30,
-    'armor_defense': 5,
-    'sword_damage': 40
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+STANCE_NORMAL = 0
+STANCE_BATTLE = 1
+STANCE_GUARD = 2
+STANCE_REST = 3
+STANCE_SLEEP = 4
+
+class Player(Base):
+    __tablename__ = 'Players'
+
+    name = Column(String(40), primary_key=True)
+    discord_id = Column(String(40))
+    ancestry = Column(Integer)
+    birthday = Column(DateTime)
+    parent_name = Column(String(40))
+    is_active = Column(Boolean)
+    stance = Column(Integer)
+    hp = Column(Integer)
+    max_hp = Column(Integer)
+    armor = Column(Integer)
+    mobility = Column(Integer)
+    dexterity = Column(Integer)
+    endurance = Column(Integer)
+    max_endurance = Column(Integer)
+    strength = Column(Integer)
+    luck = Column(Integer)
+    experience = Column(Integer)
+    magic_number = Column(Integer)
+    fighting_skill = Column(Integer)
+    hunting_skill = Column(Integer)
+    mining_skill = Column(Integer)
+    cooking_skill = Column(Integer)
+    crafting_skill = Column(Integer)
+    equipped_weapon = Column(String(100))
+    equipped_armor = Column(String(100))
+    equipped_accessory = Column(String(100))
+    guarding = Column(String(100))
+    last_attack = Column(DateTime)
+
+    def __repr__(self):
+        return f'Player(nickname={self.nickname})'
+
+IRON_SWORD = {
+    'damage': 40,
+    'weight': 100
 }
 
-ADVANCED_SWORD_PLAYER = {
-    'hp': 100,
-    'max_hp': 100,
-    'armor': 1,
-    'mobility': 30,
-    'dexterity': 60,
-    'endurance': 120,
-    'max_endurance': 120,
-    'strength': 40,
-    'luck': 1,
-    'sword_skill': 65,
-    'armor_coverage': 60,
-    'armor_defense': 35,
-    'sword_damage': 100
+SILVER_SWORD = {
+    'damage': 100,
+    'weight': 120
 }
 
-MAX_SWORD_PLAYER = {
-    'hp': 150,
-    'max_hp': 150,
-    'armor': 15,
-    'mobility': 100,
-    'dexterity': 75,
-    'endurance': 150,
-    'max_endurance': 150,
-    'strength': 75,
-    'luck': 1,
-    'sword_skill': 75,
-    'armor_coverage': 100,
-    'armor_defense': 100,
-    'sword_damge': 900
+OP_SWORD = {
+    'damage': 900,
+    'weight': 0
 }
 
-attacker = ADVANCED_SWORD_PLAYER
+LEATHER_ARMOR = {
+    'armor': 5,
+    'coverage': 30,
+    'weight': 50
+}
+
+SILVER_MAIL = {
+    'armor': 35,
+    'coverage': 60,
+    'weight': 150
+}
+
+OP_ARMOR = {
+    'armor': 200,
+    'coverage': 100,
+    'weight': 0
+}
+
+NEW_PLAYER_1 = Player(
+    hp=100,
+    max_hp=100,
+    armor=1,
+    mobility=25,
+    dexterity=25,
+    endurance=100,
+    max_endurance=100,
+    strength=15,
+    luck=1,
+    fighting_skill=0,
+    equipped_weapon='iron sword',
+    equipped_armor='leather armor'
+)
+
+ADVANCED_PLAYER_1 = Player(
+    hp=100,
+    max_hp=100,
+    armor=1,
+    mobility=30,
+    dexterity=60,
+    endurance=120,
+    max_endurance=120,
+    strength=40,
+    luck=1,
+    fighting_skill=65,
+    equipped_weapon='silver sword',
+    equipped_armor='silver mail'
+)
+
+MAX_SWORD_PLAYER = Player(
+    hp=150,
+    max_hp=150,
+    armor=15,
+    mobility=100,
+    dexterity=75,
+    endurance=150,
+    max_endurance=150,
+    strength=75,
+    luck=1,
+    fighting_skill=75,
+    equipped_weapon='silver sword',
+    equipped_armor='silver mail'
+)
+
+
+attacker = ADVANCED_PLAYER_1
+attacker_weapon = SILVER_SWORD
+attacker_armor = SILVER_MAIL
 defender = NEW_PLAYER_1
+defender_weapon = IRON_SWORD
+defender_armor = LEATHER_ARMOR
 
 def calculate_hit_chance(p1, p2):
-    offense_score = (4/3) * (0.30 * p1['dexterity'] + 0.65 * p1['sword_skill'] + 0.05 * (p1['mobility'] - 25)) + p1['luck']
-    defense_score = (4/3) * (0.65 * p2['dexterity'] + 0.15 * p2['sword_skill'] + 0.20 * (p2['mobility'] - 25)) + p2['luck']
+    offense_score = (4/3) * (0.30 * p1.dexterity + 0.65 * p1.fighting_skill + 0.05 * (p1.mobility - 25)) + p1.luck
+    defense_score = (4/3) * (0.65 * p2.dexterity + 0.15 * p2.fighting_skill + 0.20 * (p2.mobility - 25)) + p2.luck
 
     raw_percentage = int(offense_score - defense_score + 40)
     if raw_percentage < 0:
@@ -62,19 +149,19 @@ def calculate_hit_chance(p1, p2):
         return 100
     return raw_percentage
 
-def calculate_hit_damage(p1, p2):
+def calculate_hit_damage(p1, p1_weapon, p2, p2_armor):
     random_number = random.randint(0, 99)
-    total_defense = p2['armor']
-    if random_number < p2['armor_coverage']:
-        total_defense += p2['armor_defense']
+    total_defense = p2.armor
+    if random_number < p2_armor['coverage']:
+        total_defense += p2_armor['armor']
     else:
         random_number = random.randint(1, 5)
         if random_number == 1:
             return 999
-    skill_ratio = p1['sword_skill'] / 75 / 2
-    total_attack = p1['strength'] + p1['sword_damage'] * (0.5 + skill_ratio)
+    skill_ratio = p1.fighting_skill / 75 / 2
+    total_attack = p1.strength + p1_weapon['damage'] * (0.5 + skill_ratio)
     final_damage = total_attack - total_defense
-    if p2['luck'] > p1['luck']:
+    if p2.luck > p1.luck:
         final_damage -= 1
     if final_damage < 0:
         return 0
@@ -87,9 +174,9 @@ random_number = random.randint(0, 99)
 print(f'Roll: {random_number}')
 if random_number < hit_chance:
     print('P1 landed a hit on P2!')
-    hit_damage = calculate_hit_damage(attacker, defender)
+    hit_damage = calculate_hit_damage(attacker, attacker_weapon, defender, defender_armor)
     print(f'P2 took {hit_damage} damage')
-    remaining_hp = defender['hp'] - hit_damage
+    remaining_hp = defender.hp - hit_damage
     if remaining_hp <= 0:
         print(f'P2 is incapacitated')
     elif remaining_hp <= 50:
