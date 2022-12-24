b2c456929175cdd1951aa52d8ec74e656f9841b0
yehric2018 <eric.yehj@outlook.com>
2022-12-24 04:39:40 +0000

    Added some more crafting helper methods to recipe class

--- src/mmobot/db/index/models/recipe.py
+++ src/mmobot/db/index/models/recipe.py
@@ -8,23 +8,47 @@ class Recipe:
     ingredients: list
     tools: list
     handheld: str
-    skills: list
+    skills: dict
 
-    def ingredients_match(self, ingredients):
-        ingredient_map = {}
-        for ingredient in self.ingredients:
-            if ingredient.name not in ingredient_map:
-                ingredient_map[ingredient.name] = 0
+    def get_missing_ingredients(self, ingredients):
+        '''
+        From the given list of ingredients, returns a dict of ingredients missing
+        from the recipe. If all ingredients are present, an empty list is returned.
 
-    def can_craft(self, player, ingredients=[], tools=[], containers=[], handheld=None):
-        pass
+        Args: ingredients - list of ItemInstances.
+        '''
+        ingredient_counter = self._get_ingredient_counter()
+        for ingredient in ingredients:
+            if ingredient.item_id in ingredient_counter:
+                ingredient_counter[ingredient.item_id] -= 1
+                if ingredient_counter[ingredient.item_id] == 0:
+                    del ingredient_counter[ingredient.item_id]
+        return ingredient_counter
+
+    def get_crafting_skill(self, player_skills):
+        '''
+        Return a number from 0 to 70 representing the amount of surplus skill this player has
+        for crafting this recipe. If the player does not have sufficient skills to craft the
+        recipe, return -1.
+        '''
+        skill_count = len(self.skills)
+        crafting_skill = 0
+        for player_skill in player_skills:
+            if player_skill.skill_name in self.skills:
+                if player_skill.skill_level < self.skills[player_skill.skill_name]:
+                    return -1
+                crafting_skill += player_skill.skill_level - self.skills[player_skill.skill_name]
+                skill_count -= 1
+        if skill_count > 0:
+            return -1
+        return min(crafting_skill, 70)
 
     def from_yaml(item_id, yaml):
         endurance = yaml['endurance'] if 'endurance' in yaml else 0
         products = yaml['products'] if 'products' in yaml else [{'id': item_id, 'quantity': 1}]
         tools = yaml['tools'] if 'tools' in yaml else []
         handheld = yaml['handheld'] if 'handheld' in yaml else None
-        skills = yaml['skills'] if 'skills' in yaml else []
+        skills = yaml['skills'] if 'skills' in yaml else {}
         return Recipe(
             products=products,
             base_endurance=endurance,
@@ -33,3 +57,9 @@ class Recipe:
             handheld=handheld,
             skills=skills
         )
+
+    def _get_ingredient_counter(self):
+        ingredient_counter = {}
+        for ingredient in self.ingredients:
+            ingredient_counter[ingredient['id']] = ingredient['quantity']
+        return ingredient_counter
--- test/mmobot/db/index/models/test_Recipe.py
+++ test/mmobot/db/index/models/test_Recipe.py
@@ -1,13 +1,19 @@
 import os
+import pytest
 import yaml
 
 from dotenv import load_dotenv
 
 from mmobot.db.index.models import Recipe
+from mmobot.db.models import ItemInstance, PlayerSkill
+from mmobot.test.constants import TEST_ITEM_ENTITY_NUMBER
 
 load_dotenv()
 PROJECT_PATH = os.getenv('PROJECT_PATH')
 DATA_PATH = os.path.join(PROJECT_PATH, 'src', 'mmobot', 'db', 'index', 'data')
+
+TEST_HANDAXE_FILEPATH = os.path.join(DATA_PATH, 'test', 'handaxe.yaml')
+TEST_COIN_FILEPATH = os.path.join(DATA_PATH, 'test', 'copper-coin.yaml')
 
 TEST_HANDAXE_ID = 'handaxe'
 TEST_HANDAXE_RECIPE_PRODUCT = {'id': TEST_HANDAXE_ID, 'quantity': 1}
@@ -15,47 +21,100 @@ TEST_HANDAXE_RECIPE_INGREDIENT = {'id': 'stone', 'quantity': 1}
 TEST_HANDAXE_RECIPE_TOOLS = ['anvil']
 TEST_HANDAXE_RECIPE_HANDHELD = 'knife'
 TEST_HANDAXE_RECIPE_ENDURANCE = 70
-TEST_HANDAXE_RECIPE_SKILL = {'name': 'knapping', 'level': 5}
+TEST_HANDAXE_RECIPE_SKILL_KNAPPING = 5
 
 TEST_COIN_ID = 'copper-coin'
 TEST_COIN_RECIPE_PRODUCT = {'id': TEST_COIN_ID, 'quantity': 4}
 TEST_COIN_RECIPE_INGREDIENT = {'id': 'copper-ingot', 'quantity': 1}
 TEST_COIN_RECIPE_ENDRUANCE = 40
-TEST_COIN_RECIPE_SKILL_SMITHING = {'name': 'smithing', 'level': 15}
-TEST_COIN_RECIPE_SKILL_COINING = {'name': 'coining', 'level': 5}
+TEST_COIN_RECIPE_SKILL_SMITHING = 15
+TEST_COIN_RECIPE_SKILL_COINING = 5
+
+TEST_HANDAXE_INGREDIENT_LIST = [ItemInstance(id=TEST_ITEM_ENTITY_NUMBER, item_id='stone')]
 
 
-def test_Recipe_fromYaml_simpleRecipe():
-    with open(os.path.join(DATA_PATH, 'test', 'handaxe.yaml'), 'r') as f:
+@pytest.fixture
+def handaxe_yaml():
+    with open(TEST_HANDAXE_FILEPATH, 'r') as f:
         try:
-            weapon_yaml = yaml.safe_load(f)
-            recipe = Recipe.from_yaml(weapon_yaml['id'], weapon_yaml['recipes'][0])
-            assert recipe.base_endurance == TEST_HANDAXE_RECIPE_ENDURANCE
-            assert len(recipe.products) == 1
-            assert recipe.products[0] == TEST_HANDAXE_RECIPE_PRODUCT
-            assert len(recipe.ingredients) == 1
-            assert recipe.ingredients[0] == TEST_HANDAXE_RECIPE_INGREDIENT
-            assert recipe.tools == TEST_HANDAXE_RECIPE_TOOLS
-            assert recipe.handheld == TEST_HANDAXE_RECIPE_HANDHELD
-            assert len(recipe.skills) == 1
-            assert recipe.skills[0] == TEST_HANDAXE_RECIPE_SKILL
+            return yaml.safe_load(f)
         except yaml.YAMLError as exc:
             assert f'Encountered yaml error: {exc}' is False
 
 
-def test_Recipe_fromYaml_withProducts_noToolsOrHandhelds():
-    with open(os.path.join(DATA_PATH, 'test', 'copper-coin.yaml'), 'r') as f:
+@pytest.fixture
+def coin_yaml():
+    with open(TEST_COIN_FILEPATH, 'r') as f:
         try:
-            coin_yaml = yaml.safe_load(f)
-            recipe = Recipe.from_yaml(coin_yaml['id'], coin_yaml['recipes'][0])
-            assert len(recipe.products) == 1
-            assert recipe.products[0] == TEST_COIN_RECIPE_PRODUCT
-            assert len(recipe.ingredients) == 1
-            assert recipe.ingredients[0] == TEST_COIN_RECIPE_INGREDIENT
-            assert len(recipe.tools) == 0
-            assert recipe.handheld is None
-            assert len(recipe.skills) == 2
-            assert TEST_COIN_RECIPE_SKILL_SMITHING in recipe.skills
-            assert TEST_COIN_RECIPE_SKILL_COINING in recipe.skills
+            return yaml.safe_load(f)
         except yaml.YAMLError as exc:
             assert f'Encountered yaml error: {exc}' is False
+
+
+@pytest.fixture
+def coin_crafter_skills():
+    return [
+        PlayerSkill(skill_name='smithing', skill_level=TEST_COIN_RECIPE_SKILL_SMITHING),
+        PlayerSkill(skill_name='coining', skill_level=TEST_COIN_RECIPE_SKILL_COINING)
+    ]
+
+
+def test_Recipe_fromYaml_simpleRecipe(handaxe_yaml):
+    recipe = Recipe.from_yaml(handaxe_yaml['id'], handaxe_yaml['recipes'][0])
+    assert recipe.base_endurance == TEST_HANDAXE_RECIPE_ENDURANCE
+    assert len(recipe.products) == 1
+    assert recipe.products[0] == TEST_HANDAXE_RECIPE_PRODUCT
+    assert len(recipe.ingredients) == 1
+    assert recipe.ingredients[0] == TEST_HANDAXE_RECIPE_INGREDIENT
+    assert recipe.tools == TEST_HANDAXE_RECIPE_TOOLS
+    assert recipe.handheld == TEST_HANDAXE_RECIPE_HANDHELD
+    assert len(recipe.skills) == 1
+    assert recipe.skills['knapping'] == TEST_HANDAXE_RECIPE_SKILL_KNAPPING
+
+
+def test_Recipe_fromYaml_withProducts_noToolsOrHandhelds(coin_yaml):
+    recipe = Recipe.from_yaml(coin_yaml['id'], coin_yaml['recipes'][0])
+    assert len(recipe.products) == 1
+    assert recipe.products[0] == TEST_COIN_RECIPE_PRODUCT
+    assert len(recipe.ingredients) == 1
+    assert recipe.ingredients[0] == TEST_COIN_RECIPE_INGREDIENT
+    assert len(recipe.tools) == 0
+    assert recipe.handheld is None
+    assert len(recipe.skills) == 2
+    assert recipe.skills['smithing'] == 15
+    assert recipe.skills['coining'] == 5
+
+
+def test_Recipe_getMissingIngredients_matchReturnsEmpty(handaxe_yaml):
+    recipe = Recipe.from_yaml(handaxe_yaml['id'], handaxe_yaml['recipes'][0])
+    assert len(recipe.get_missing_ingredients(TEST_HANDAXE_INGREDIENT_LIST)) == 0
+
+
+def test_Recipe_getMissingIngredients_missingIngredientReturnsNonempty(handaxe_yaml):
+    recipe = Recipe.from_yaml(handaxe_yaml['id'], handaxe_yaml['recipes'][0])
+    missing_ingredients = recipe.get_missing_ingredients([])
+    assert len(missing_ingredients) == 1
+    assert missing_ingredients['stone'] == 1
+
+
+def test_Recipe_getMissingIngredients_surplusIngredientsReturnsEmpty(handaxe_yaml):
+    recipe = Recipe.from_yaml(handaxe_yaml['id'], handaxe_yaml['recipes'][0])
+    ingredients = TEST_HANDAXE_INGREDIENT_LIST + [ItemInstance(id=50, item_id='stone')]
+    assert len(recipe.get_missing_ingredients(ingredients)) == 0
+
+
+def test_Recipe_getCraftingSkill_barelySufficientSkill(coin_yaml, coin_crafter_skills):
+    recipe = Recipe.from_yaml(coin_yaml['id'], coin_yaml['recipes'][0])
+    assert recipe.get_crafting_skill(coin_crafter_skills) == 0
+
+
+def test_Recipe_getCraftingSkill_insufficientSkill(coin_yaml, coin_crafter_skills):
+    recipe = Recipe.from_yaml(coin_yaml['id'], coin_yaml['recipes'][0])
+    assert recipe.get_crafting_skill(coin_crafter_skills[:1]) == -1
+
+
+def test_Recipe_getCraftingSkill_surplusSkill(coin_yaml, coin_crafter_skills):
+    coin_crafter_skills[0].skill_level = 50
+    coin_crafter_skills[1].skill_level = 10
+    recipe = Recipe.from_yaml(coin_yaml['id'], coin_yaml['recipes'][0])
+    assert recipe.get_crafting_skill(coin_crafter_skills) == 40
